4ca7de0b281239d97c377553604f40421e89e6ed
Tim Beermann <beermann@osism.tech>
2021-10-15 13:57:46 +0200

    github clone script updated to use gh instead of curl
    Signed-off-by: Tim Beermann <beermann@osism.tech>

--- github/clone-all-repositories.sh
+++ github/clone-all-repositories.sh
@@ -3,9 +3,7 @@
 # Script to clone all repositories from one organization into the current directory
 #
 ####################################################################################################
-token=<put_your_github_access_token_here>
-orga=osism
+org=osism
 
-for i in $(curl -s -u tibeer:${token} -H "Accept: application:vnd.github.v3+json" 'https://api.github.com/orgs/'${orga}'/repos?per_page=100' | jq '.[].name' | sed 's/"//g'); do
-  git clone git@github.com:${orga}/${i}.git
-done
+gh repo list $org --limit 100 --json isArchived,sshUrl | jq '.[] | select( .isArchived == false)' | jq -r '.["sshUrl"]' | xargs -L1 git clone
+
